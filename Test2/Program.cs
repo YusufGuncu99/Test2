323c03b648e6af6e7d897a610d78cbf2573e4429
Yusuf <Yusuf@MATEBOOK>
2023-06-18 17:14:36 +0300

    Add project files.

---
+++ Test2/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+int a;
+int i = 0;
+string x = "a";
+
+
+Değer:
+try
+{
+    if (i < 100)
+    {
+        int y = int.Parse(x);
+    }
+   
+    else
+    {
+        a = 1; 
+    }
+       
+
+
+}
+catch
+{
+   
+    Console.WriteLine("For kullanmadım.");
+    i++;
+
+    goto Değer;
+}
